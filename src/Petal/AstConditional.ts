dcf2ac0971d5d36f88b7ef16c50878b09b5561af
Kayateia <github@kayateia.net>
2016-07-18 17:26:20 -0700

    Conditional support

--- src/Petal/AstConditional.ts
+++ src/Petal/AstConditional.ts
@@ -8,6 +8,7 @@ import { AstNode } from "./AstNode";
 import { parse } from "./Parser";
 import { Runtime } from "./Runtime";
 import { Value } from "./Value";
+import { Compiler } from "./Compiler";
 
 export class AstConditional extends AstNode {
 	constructor(parseTree: any) {
@@ -17,6 +18,33 @@ export class AstConditional extends AstNode {
 		if (parseTree.alternate)
 			this.alternate = parse(parseTree.alternate);
 		this.statement = parseTree.type === "IfStatement";
+	}
+
+	public compile(compiler: Compiler): void {
+		// Evaluate the condition first.
+		this.test.compile(compiler);
+
+		// What was the result?
+		let skipConsequent = compiler.newLabel(this);
+		let skipToEnd = compiler.newLabel(this);
+		compiler.emit("Conditional branch", this, (runtime: Runtime) => {
+			let testResult = Value.PopAndDeref(runtime);
+			if (!testResult)
+				runtime.gotoPC(skipConsequent);
+		});
+
+		// Compile the consequent.
+		this.consequent.compile(compiler);
+		compiler.emit("Conditional skip alternate", this, (runtime: Runtime) => {
+			runtime.gotoPC(skipToEnd);
+		});
+
+		// And the alternate, if we have one.
+		skipConsequent.pc = compiler.pc;
+		if (this.alternate)
+			this.alternate.compile(compiler);
+
+		skipToEnd.pc = compiler.pc;
 	}
 
 	/*public execute(runtime: Runtime): void {
