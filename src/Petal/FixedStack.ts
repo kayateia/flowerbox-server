11f15c6d712af736fda797160cd425ca287257dc
Kayateia <github@kayateia.net>
2016-07-23 12:38:50 -0700

    Add FixedStack util class for a fast stack

---
+++ src/Petal/FixedStack.ts
@@ -0,0 +1,59 @@
+/*
+	Flowerbox
+	Copyright (C) 2016 Kayateia
+	For license info, please see notes/gpl-3.0.txt under the project root.
+*/
+
+import { RuntimeException } from "./Exceptions";
+
+export class FixedStack<T> {
+	constructor() {
+		this._stack = [];
+		this._sp = 0;
+	}
+
+	public push(value: T): void {
+		if (this._stack.length <= this._sp) {
+			for (let i=0; i<10000; ++i)
+				this._stack.push(null);
+		}
+
+		this._stack[this._sp++] = value;
+	}
+
+	public pop(): T {
+		if (this._sp === 0)
+			throw new RuntimeException("Stack underflow");
+
+		return this._stack[--this._sp];
+	}
+
+	public get empty(): boolean {
+		return this._sp === 0;
+	}
+
+	public clear(): void {
+		this._sp = 0;
+	}
+
+	public popMany(count: number): void {
+		if (this._sp < count)
+			throw new RuntimeException("Stack underflow");
+
+		this._sp -= count;
+	}
+
+	public get(index: number): T {
+		if (this._sp < (1+index))
+			throw new RuntimeException("Stack underflow");
+
+		return this._stack[this._sp - (1+index)];
+	}
+
+	public get count(): number {
+		return this._sp;
+	}
+
+	private _stack: T[];
+	private _sp: number;
+}
