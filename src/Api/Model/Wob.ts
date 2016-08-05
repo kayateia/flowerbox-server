68592bfd06ff635e44f78e8ddd7c8fa504cf788b
Kayateia <github@kayateia.net>
2016-08-05 19:29:32 -0400

    Convert the property getter to async, and add the info getter to the API

--- src/Api/Model/Wob.ts
+++ src/Api/Model/Wob.ts
@@ -1,11 +1,12 @@
 /*
 	Flowerbox
-	Copyright (C) 2016 Dove
+	Copyright (C) 2016 Dove, Kayateia
 	For license info, please see notes/gpl-3.0.txt under the project root.
 */
 
 import { ModelBase } from "./ModelBase";
 
+// For returning properties on a wob.
 export class Property extends ModelBase {
 	constructor(id: number, property: string, value: any) {
 		super(true);
@@ -17,4 +18,48 @@ export class Property extends ModelBase {
 	public id: number;
 	public property: string;
 	public value: any;
+}
+
+// For returning the basic info about a wob.
+export class Info extends ModelBase {
+	constructor(id: number, base: number, container: number,
+			name: string, desc: string, globalid: string,
+			properties?: AttachedItem[], verbs?: AttachedItem[]) {
+		super(true);
+
+		this.id = id;
+		this.base = base;
+		this.container = container;
+
+		this.name = name;
+		this.desc = desc;
+		this.globalid = globalid;
+
+		this.properties = properties;
+		this.verbs = verbs;
+	}
+
+	// Intrinsic properties
+	public id: number;
+	public base: number;
+	public container: number;
+
+	// Common named properties
+	public name: string;
+	public desc: string;
+	public globalid: string;
+
+	// List of properties and verbs, by wob ID.
+	public properties: AttachedItem[];
+	public verbs: AttachedItem[];
+}
+
+export class AttachedItem {
+	constructor(sourceid: number, value: string) {
+		this.sourceid = sourceid;
+		this.value = value;
+	}
+
+	public sourceid: number;
+	public value: string;
 }
--- src/Api/Routes/World.ts
+++ src/Api/Routes/World.ts
@@ -1,6 +1,6 @@
 /*
 	Flowerbox
-	Copyright (C) 2016 Dove
+	Copyright (C) 2016 Dove, Kayateia
 	For license info, please see notes/gpl-3.0.txt under the project root.
 */
 
@@ -10,29 +10,54 @@ import { RouterBase } from "./RouterBase";
 import { ModelBase } from "../Model/ModelBase";
 import * as Wob from "../Model/Wob";
 import * as Petal from "../../Petal/All";
+import * as World from "../../World/All";
 
 export class WorldRouter extends RouterBase {
 	constructor() {
 		super();
 
-		this.router.get("/wob/:id/property/:name", (rq,rs,n) => { this.getProperty(rq, rs, n); });
+		this.router.get("/wob/:id/property/:name", (rq,rs,n) => { this.asyncWrapper(rq,rs,n, ()=>this.getProperty(rq,rs,n)); });
+		this.router.get("/wob/:id/info", (rq,rs,n) => { this.asyncWrapper(rq,rs,n,()=>this.info(rq,rs,n)); });
 	}
 
-	private getProperty(req, res, next): void {
-		this.world.getWob(parseInt(req.params.id, 10))
-			.then(w => {
-				return w.getPropertyI(req.params.name, this.world);
-			})
-			.then(pv => {
-				return res.json(new Wob.Property(
-					req.params.id,
-					req.params.name,
-					Petal.ObjectWrapper.Unwrap(pv)
-				));
-			})
-			.catch(err => {
-				return res.json(new ModelBase(false, err));
-			});
+	private async getProperty(req, res, next): Promise<any> {
+		let id = req.params.id;
+		let name = req.params.name;
+
+		let wob = await this.world.getWob(parseInt(id, 10));
+		if (!wob)
+			return res.json(new ModelBase(false, "Unknown wob ID"));
+
+		let prop = await wob.getPropertyI(name, this.world);
+		return res.json(new Wob.Property(
+			prop.wob,
+			name,
+			Petal.ObjectWrapper.Unwrap(prop)
+		));
+	}
+
+	private async info(req, res, next): Promise<any> {
+		let id = req.params.id;
+
+		let wob = await this.world.getWob(parseInt(id, 10));
+		if (!wob)
+			return res.json(new ModelBase(false, "Unknown wob ID"));
+
+		let base = wob.base;
+		let container = wob.container;
+
+		let name = await wob.getPropertyI(World.WobProperties.Name, this.world);
+		let desc = await wob.getPropertyI(World.WobProperties.Description, this.world);
+		let globalid = wob.getProperty(World.WobProperties.GlobalId);
+
+		let properties = await wob.getPropertyNamesI(this.world);
+		let verbs = await wob.getVerbNamesI(this.world);
+
+		let rv = new Wob.Info(id, base, container, name.value, desc.value, globalid,
+			properties.map(p => new Wob.AttachedItem(p.wob, p.value)),
+			verbs.map(v => new Wob.AttachedItem(v.wob, v.value)));
+
+		return res.json(rv);
 	}
 }
 
