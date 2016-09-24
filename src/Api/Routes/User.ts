9336f661ec272f5b4b592f0d4d9cb49c62d5381b
Kayateia <github@kayateia.net>
2016-09-23 22:12:39 -0700

    Set return codes on the failure paths in the user APIs.

--- src/Api/Routes/User.ts
+++ src/Api/Routes/User.ts
@@ -36,13 +36,17 @@ export class UserRouter extends RouterBase {
 		console.log(password);
 
 		let players = await this.world.getWobsByGlobalId([userId]);
-		if (players.length !== 1)
+		if (players.length !== 1) {
+			res.status(401);
 			return new ModelBase(false, "Username or password was invalid.");
+		}
 		let player = players[0];
 
 		let pwhash = player.getProperty(World.WobProperties.PasswordHash);
-		if (!pwhash || pwhash.value != password)
+		if (!pwhash || pwhash.value != password) {
+			res.status(401);
 			return new ModelBase(false, "Username or password was invalid.");
+		}
 
 		let tokenContents = new Token(userId, player.id, password, false);
 
@@ -65,8 +69,10 @@ export class UserRouter extends RouterBase {
 
 	public async playerInfo(req, res, next): Promise<ModelBase> {
 		let wob = await this.world.getWob(this.token.wobId);
-		if (!wob)
+		if (!wob) {
+			res.status(404);
 			return new ModelBase(false, "Can't find user wob");
+		}
 
 		res.json(await WobCommon.GetInfo(wob, wob, this.token.admin, this.world));
 		return null;
